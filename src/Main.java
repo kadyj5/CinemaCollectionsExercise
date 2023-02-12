76645351aa7412e33d0b2c014773fff5ba8fa3d8
Karolina Dyjak <karolina.dyjak5@gmail.com>
2023-02-12 16:30:53 +0100

    Collections

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+
         Theatre theatre = new Theatre("Cinema City", 8, 12);
-        theatre.getSeats();
+
+        if(theatre.reserveSeat("D12")) {
+            System.out.println("Please pay for D12");
+        } else {
+            System.out.println("Seat is already taken");
+        }
+        if(theatre.reserveSeat("D12")) {
+            System.out.println("Please pay for D12");
+        } else {
+            System.out.println("Seat is already taken");
+        }
+
+        if(theatre.reserveSeat("B13")) {
+            System.out.println("Please pay for B13");
+        } else {
+            System.out.println("Seat is already taken");
+        }
+
+    }
+
+    public static void  printList(List<Theatre.Seat> list) {
+        for(Theatre.Seat seat : list) {
+            System.out.println(" " + seat.getSeatNumber() + " " + seat.getPrice());
+        }
+        System.out.println();
+        System.out.println("=".repeat(25));
     }
 }
--- src/Theatre.java
+++ src/Theatre.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Theatre {
     private final String theatreName;
@@ -10,7 +9,13 @@ public class Theatre {
         int lastRow = 'A' + (numRows - 1);
         for(char row = 'A'; row <= lastRow; row++){
             for(int seatNum  = 1; seatNum <= seatsPerRow; seatNum++) {
-                Seat seat = new Seat(row + String.format("%02d", seatNum));
+                double price = 12.00;
+                if((row < 'D') && (seatNum >= 4 && seatNum <= 9)) {
+                    price = 14.00;
+                } else if((row > 'F') || (seatNum < 4 && seatNum > 9)) {
+                    price = 7.00;
+                }
+                Seat seat = new Seat(row + String.format("%02d", seatNum), price);
                 seats.add(seat);
             }
         }
@@ -21,35 +26,42 @@ public class Theatre {
     }
 
     public boolean reserveSeat(String seatNumber) {
-        Seat requestedSeat = null;
-        for(Seat seat : seats) {
-            if(seat.getSeatNumber().equals(seatNumber)) {
-                requestedSeat  = seat;
-                break;
-            }
-        }
-        if(requestedSeat == null) {
-            System.out.println("There is no seat " + seatNumber);
+        Seat requestedSeat = new Seat(seatNumber, 0);
+        int foundSeat = Collections.binarySearch(seats,requestedSeat,null);
+        if(foundSeat >= 0) {
+            return seats.get(foundSeat).reserve();
+        } else {
+            System.out.println("There is n seat " + seatNumber);
             return false;
         }
-        return requestedSeat.reserve();
-
     }
 
-    // testing
-    public void getSeats() {
-        for (Seat seat : seats) {
-            System.out.println(seat.getSeatNumber());
+    public Collection<Seat> getSeats() {
+        return seats;
+    }
+
+    public static void printList(List<Seat> list) {
+        for(Seat seat : list) {
+            System.out.println(" " + seat.getSeatNumber());
         }
+        System.out.println();
+        System.out.println("=".repeat(20));
     }
 
 
-    private class Seat {
+    public class Seat implements Comparable<Seat>{
         private final  String seatNumber;
         private boolean reserved = false;
+        private double price;
 
-        public Seat(String seatNumber) {
+        public Seat(String seatNumber, double price) {
             this.seatNumber = seatNumber;
+            this.price = price;
+        }
+
+        @Override
+        public int compareTo(Seat seat) {
+            return this.seatNumber.compareToIgnoreCase(seat.getSeatNumber());
         }
 
         public boolean reserve() {
@@ -74,6 +86,10 @@ public class Theatre {
 
         public String getSeatNumber() {
             return seatNumber;
+        }
+
+        public double getPrice() {
+            return price;
         }
     }
 }
